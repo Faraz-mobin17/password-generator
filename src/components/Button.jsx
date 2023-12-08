d34ef3e4cf12051ec3a55da525ce6aeaa0af62c6
Faraz-mobin17 <faraz.mobin17@gmail.com>
2023-12-08 20:26:44 +0530

    added transition

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ function Button({ generatePassword }) {
   return (
     <button
       type="button"
-      className=" bg-neon-green text-dark-grey text-base sm:text-body  py-[1.125rem] sm:py-5 w-full flex justify-center items-center gap-4 border-2 box-border border-dark-grey hover:bg-dark-grey hover:text-neon-green hover:border-neon-green mt-[32px]"
+      className=" bg-neon-green text-dark-grey text-base sm:text-body  py-[1.125rem] sm:py-5 w-full flex justify-center items-center gap-4 border-2 box-border border-dark-grey hover:bg-dark-grey hover:text-neon-green hover:border-neon-green mt-[32px] transition-colors"
       onClick={generatePassword}
     >
       <span>GENERATE</span>
