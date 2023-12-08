33628ac4c91c2bf7117271440d112457bd9c7ff2
Faraz-mobin17 <faraz.mobin17@gmail.com>
2023-12-08 19:47:36 +0530

    made Butotn component

---
+++ src/components/Button.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ArrowRight from "../assets/images/icon-arrow-right";
+function Button({ generatePassword }) {
+  return (
+    <button
+      type="button"
+      className=" bg-neon-green text-dark-grey text-base sm:text-body  py-[1.125rem] sm:py-5 w-full flex justify-center items-center gap-4 border-2 box-border border-dark-grey hover:bg-dark-grey hover:text-neon-green hover:border-neon-green mt-[32px]"
+      onClick={generatePassword}
+    >
+      <span>GENERATE</span>
+      <ArrowRight className="text-inherit" />
+    </button>
+  );
+}
+
+export default Button;
